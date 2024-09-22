ef78b138f8fbdf0552f5d88d4f00519d74608796
tomeytran <tmtri310251@gmail.com>
2024-09-22 22:56:26 +0700

    xong

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 let sampleText = "Hello world";
+let sampleArr = sampleText.split("");
+let beforeArr = document.getElementById("fakeText").value.split("");
+console.log(sampleArr.length);
+function changeText() {
+	let afterArr = document.getElementById("fakeText").value.split("");
+	console.log("typing");
+	console.log(afterArr.length);
+	if (afterArr.length > beforeArr.length) {
+		for (let i = 0; i < afterArr.length; i++) {
+			beforeArr[i] = sampleArr[i];
+		}
+		console.log(beforeArr);
+	} else {
+		beforeArr.length = afterArr.length;
+	}
+	document.getElementById("fakeText").value = beforeArr.join("");
+}
